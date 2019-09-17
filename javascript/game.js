c9f0531c11b229cfec4724aca10c72cc9b8f7868
Rebecca Leggett <roboticsgirl60@gmail.com>
2019-09-17 10:47:18 -0700

    added code for moving left, right, and up

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,46 @@
+/*
+TO DO:
+	add keyboard input
+	place bug somewhere that they have to collect
+	write code so they can collect the bug
+	should the bug move?
+	bug might be optional, just moving the bird might be good enough
+	make a border for the game
+	fix the bird so it doesn't go off the screen (might be fixed by changing bird size so it's not so big and maybe not letting it enter the squares on the side?)
+		also make the grid a 8 x 8 grid and save the other squares for the border and put the border as far as you can go to the wall
+		could also just increase the size of the game board by one square
+	fix the scale for the bird and the game window (was on the last quiz, is that on the transform stuff?)
+		use setTransform for this, don't redraw or rescale the image again
+		could setTransform be used to change the scale and then continue with what is being done?
+	change the set interval loop to a game loop thing?? (optional, just make it work first, then optimize)
+	game score thing?
+	use set transform instead of manually setting position
+		still need to do calculations, but seting the x and y manually might not be the best way?
+	game sound???
+		like something when an item is collected
+		
+	need to add icon
+		
+	maybe remove the parameters of the function
+		it can just access them on its own and is doing that to change it right now
+	
+	
+	mirror bird image for when going right and change spites if it is on the other one? or just rerender the same sprite?
+	
+	
+	border art:
+		flowers?? trees? bush?
+		make 40 pixels long, not sure how wide (each square is 40 pixels)
+		just make one really long element that covers the whole grid length
+		maybe just make a small section and use the selection tool to make duplicates until the right length is reached
+		
+		
+	future art update, have the bird be flying and land in each square if there's a long enough pause between key presses?
+		have one image for wings open, have another for flapping
+*/
+
+
+
 var gameport = document.getElementById("gameport");
 var renderer = PIXI.autoDetectRenderer({width: 400, height: 400, backgroundColor: 0xf5fcf7});
 var stage = new PIXI.Container();
@@ -12,15 +55,18 @@ bird.anchor.y = 0.5;
 bird.position.x = 200;
 bird.position.y = 200;
 
+bird.scale.x = 0.65;
+bird.scale.y = 0.65;
+
 stage.addChild(bird);
 
 
-game_map = [10][10]
+game_map = [10][10];
 // squares are numbered 0-9
-current_x_square = 4
-current_y_square = 4
-current_location_x = 180
-current_location_y = 180
+current_x_square = 4;
+current_y_square = 4;
+current_location_x = 180;
+current_location_y = 180;
 
 // for a 400 x 400 grid with 10 spaces each way, each square should be 40 pixels
 // need to place in the middle of those squares, so start off with 40 * square_number + 20?
@@ -39,7 +85,8 @@ function moveDown(current_x, current_y)
 {
 	// checks to see if already in top square
 	// 380 is the center of the top square
-	if (current_y < 380)
+	//if (current_y < 380)
+	if (current_y < 340)
 	{
 		current_location_y = current_y + 40;
 		current_y_square += 1;
@@ -47,9 +94,43 @@ function moveDown(current_x, current_y)
 	}
 }
 
+function moveLeft(current_x, current_y)
+{
+		if (current_x > 60)
+	{
+		current_location_x = current_x - 40;
+		current_x_square -= 1;
+		bird.position.x = current_location_x;
+	}
+}
+
+function moveRight(current_x, current_y)
+{
+		if (current_x < 340)
+	{
+		current_location_x = current_x + 40;
+		current_x_square += 1;
+		bird.position.x = current_location_x;
+	}
+}
+
+function moveUp(current_x, current_y)
+{
+	//if (current_y > 20)
+	if (current_y > 60)
+	{
+		current_location_y = current_y - 40;
+		current_y_square -= 1;
+		bird.position.y = current_location_y;
+	}
+}
+
 
 setInterval(function(){
-	moveDown(current_location_x, current_location_y)
+	//moveDown(current_location_x, current_location_y);
+	//moveUp(current_location_x, current_location_y);
+	//moveRight(current_location_x, current_location_y);
+	moveLeft(current_location_x, current_location_y);
 }, 3000);
 
 function animate()
